653bfb739e66587109d5f17b136aa214e10cf651
Minhquan13041999 <24211TT1178@mail.tdc.edu.vn>
2025-11-15 11:56:35 +0700

    Upload full project for layoutKetQua_MinhQuan branch

---
+++ app/src/main/java/vn/edu/tdc/doannhom3/KetQua.kt
@@ -0,0 +1,12 @@
+package vn.edu.tdc.doannhom3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KetQua : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ket_qua)
+
+    }
+}
